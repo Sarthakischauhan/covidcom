5134d257bd05fcaf78e7312cfaf6da064308564c
Sarthakischauhan <sarthakchauhann@gmail.com>
2021-05-16 14:00:13 +0530

    heroku rum test

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,14 @@
 from app import user
 from flask import Flask
 import os 
-from flask_googlemaps import GoogleMaps
 
 
 
 app = Flask(__name__)
-GoogleMaps(app=app)
 
 if app.config["ENV"]=="development":
 	app.config.from_object("config.DevelopmentConfig")
-	app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-	app.config["GOOGLEMAPS_KEY"] = os.environ.get("GOOGLEMAPS_KEY")
+	# app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 
 
 from app.user.routes import users_bp
